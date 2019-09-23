925f02b75b6dbde4fe7901f00f8368c30e50d890
artur <artik222012@gmail.com>
2019-09-23 17:25:27 +0500

    create settings fragment

---
+++ app/src/main/java/com/softartdev/noteroom/ui/settings/SettingsActivity.kt
@@ -0,0 +1,6 @@
+package com.softartdev.noteroom.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import com.softartdev.noteroom.R
+
+class SettingsActivity : AppCompatActivity(R.layout.settings_activity)
