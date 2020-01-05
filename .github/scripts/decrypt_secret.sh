1ca2fec53b6d7ea061b0fbb01297d24a6beee98b
artur <artik222012@gmail.com>
2020-01-05 06:17:41 +0500

    Adding github action step

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Decrypt the file
+# --batch to prevent interactive command --yes to assume "yes" for questions
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./keystore.properties ./.github/secrets/keystore.properties.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./note_room_key_store.jks ./.github/secrets/note_room_key_store.jks.gpg
