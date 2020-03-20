d53881276867601fa8f485c70f64951dc28c1827
artur <artik222012@gmail.com>
2020-03-21 04:55:47 +0500

    update libs

--- app/src/main/java/com/softartdev/noteroom/util/log/TimberKoinLogger.kt
+++ app/src/main/java/com/softartdev/noteroom/util/log/TimberKoinLogger.kt
@@ -8,10 +8,16 @@ import timber.log.Timber
 
 class TimberKoinLogger(level: Level) : Logger(level) {
 
-    override fun log(level: Level, msg: MESSAGE) = when (level) {
+    override fun log(level: Level, msg: MESSAGE) {
+        if (this.level <= level) {
+            logOnLevel(msg, level)
+        }
+    }
+
+    private fun logOnLevel(msg: MESSAGE, level: Level) = when (level) {
         Level.DEBUG -> Timber.tag(KOIN_TAG).d(msg)
         Level.INFO -> Timber.tag(KOIN_TAG).i(msg)
         Level.ERROR -> Timber.tag(KOIN_TAG).e(msg)
-        Level.NONE -> Unit
+        else -> Timber.tag(KOIN_TAG).e(msg)
     }
 }
